c136bbfc0680091b4d90b6095edb8b8cbbf85545
Martin Gerdzhev <martin.gerdzhev@mail.mcgill.ca>
2017-03-13 16:25:29 -0400

    Started a js annotator based on bbox-annotator and webImageAnnotator

--- WebImageAnnotator/src/martingerdzhev/ImageAnnotatorBundle/Filter/FileFilter.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace martingerdzhev\ImageAnnotatorBundle\Filter;
-
-abstract class FileFilter
-{
-	const FILTER_NOFILTER = "image_annotator.filter.nofilter";
-	const FILTER_IMAGE = "image_annotator.filter.image";
-}
--- WebImageAnnotator/src/martingerdzhev/ImageAnnotatorBundle/ImageAnnotatorBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace martingerdzhev\ImageAnnotatorBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ImageAnnotatorBundle extends Bundle
-{
-}
